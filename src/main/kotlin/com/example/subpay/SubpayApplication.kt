4cf5ec32817037306a0771a2ecd0305474892f64
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2025-05-02 15:10:22 +0900

    feat: 정기 구독 결제 서비스 init

---
+++ src/main/kotlin/com/example/subpay/SubpayApplication.kt
@@ -0,0 +1,11 @@
+package com.example.subpay
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SubpayApplication
+
+fun main(args: Array<String>) {
+	runApplication<SubpayApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/subpay/SubpayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.subpay
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SubpayApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
